de72aaa8f9bf0fc4ca402e7801c92431a49d7243
Nate Jones <nate@endot.org>
2015-03-28 15:19:05 -0700

    create brain go block to manage data

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [http-kit "2.1.18"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler lollipop.handler/app}
--- src/lollipop/handler.clj
+++ src/lollipop/handler.clj
@@ -2,11 +2,35 @@
   (:gen-class)
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [clojure.core.async :as async :refer [go <!! put! chan <! >!]]
+            [clojure.string :as str]
             [org.httpkit.server :refer [run-server]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(def brain-chan (chan))
+
+(go (loop [brain {}]
+      (let [{:keys [type resp k v]} (<! brain-chan)]
+        (println (str "received " type " for " k " = " v))
+        (case type
+          :put (do (recur (assoc brain k v)))
+          :get (do (>! resp (get brain k "default value")) (recur brain))))))
+
+; put in some starter values
+(put! brain-chan {:type :put :k "one" :v "one one one one"})
+(put! brain-chan {:type :put :k "two" :v "twodytwody"})
+
+(defn get-data
+  [request]
+  (let [server-name (:server-name request)
+        subdom (first (str/split server-name #"\."))
+        resp-chan (chan)
+        _ (put! brain-chan {:type :get :k subdom :resp resp-chan})
+        value (<!! resp-chan)]
+    value))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" request (get-data request))
   (route/not-found "Not Found"))
 
 (def app
