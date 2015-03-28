6922f6c9e301f6fadb666e7f665b6f211a09150b
Nate Jones <nate@endot.org>
2015-03-27 20:34:50 -0700

    make lein uberjar work

--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [http-kit "2.1.18"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler lollipop.handler/app}
+  :main lollipop.handler
+  :aot [lollipop.handler]
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
--- src/lollipop/handler.clj
+++ src/lollipop/handler.clj
@@ -1,6 +1,8 @@
 (ns lollipop.handler
+  (:gen-class)
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [org.httpkit.server :refer [run-server]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
@@ -9,3 +11,6 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn -main [port]
+  (run-server app {:port (Integer. port)}))
