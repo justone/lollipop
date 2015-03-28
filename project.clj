d26904b6e2edca417e4d0bb4eac4d5ae0d6d70bd
Nate Jones <nate@endot.org>
2015-03-27 20:39:36 -0700

    name/url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lollipop "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Something of an 'echo' server.  You give it something and it will return the same."
+  :url "https://github.com/justone/lollipop"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
